7bf184d21d24b6b63fba457151dd2d98b4f9293c
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-03-04 18:19:55 +0700

    init:create project

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import React from "react";
+
+export default function middleware(req: NextRequest) {
+  const url = req.url;
+  const isLogin = true;
+  const server = "http://localhost:3000";
+  if (url.includes("/stock")) {
+    if (isLogin) {
+      return NextResponse.next(); //ถ้า login ผ่านก็ให้เข้าหน้านั้นได้
+    } else {
+      return NextResponse.redirect(`${server}`); //ถ้า login ไม่ผ่าน ให้ไปหน้า login
+    }
+  }
+  return NextResponse.next();
+}
