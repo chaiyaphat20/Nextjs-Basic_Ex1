af5afc6f8d687165860a6a409b3751437ad3c4f2
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-03-05 20:57:43 +0700

    feat: swr

--- src/pages/api/youtubes.ts
+++ src/pages/api/youtubes.ts
@@ -1,8 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import data from "./foodData.json";
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  res.status(200).json({ method: req.method });
+  setTimeout(() => {
+    res.status(200).json(data);
+  }, 2000);
 }
--- src/pages/swr/youtubes.tsx
+++ src/pages/swr/youtubes.tsx
@@ -1,17 +1,43 @@
 import useSWR from "swr";
-import axios from 'axios';
+import axios from "axios";
 export default function ClintSide() {
-  const fetcherFetch = (url:string) => fetch(url).then(r => r.json())
-  const fetcherAxios = (url:string) => axios.get(url).then(res => res.data)
-  const url =
-    "https://jsonplaceholder.typicode.com/todos";
-  const { data, error } = useSWR(url, fetcherFetch);
-	console.log(error)
+  const fetcherFetch = (url: string) => fetch(url).then((r) => r.json());
+  const fetcherAxios = (url: string) => axios.get(url).then((res) => res.data);
+  const url = "/api/youtubes";
+  const { data, error, mutate } = useSWR(url, fetcherFetch);
+  console.log(error);
 
   return (
     <div>
       <div>Clint-Side</div>
-      <span>{JSON.stringify(data)}</span>
+      <div>
+        <h1>Movies</h1>
+        <button
+          onClick={() => {
+            mutate({
+              ...data,
+              youtubes: [...data.youtubes, { title: "xxxx", subtitle: "yyyy" }],
+            });
+          }}
+        >
+          Mutate
+        </button>
+        <ul>
+          {data &&
+            data.youtubes.map((movie: any, index: number) => {
+              return (
+                <li key={`${index}`}>
+                  <img
+                    src={movie.youtube_image}
+                    height={20}
+                    style={{ marginRight: 16 }}
+                  />
+                  {`${index + 1}. ${movie.title} : ${movie.subtitle}`}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </div>
   );
 }
